33fa46e3c90c8a8a9062012cf02eda505b31b9bf
NekoIay <77748612+NekoIay@users.noreply.github.com>
2021-08-26 18:17:12 +0300

    no message

---
+++ Assets/Scripts/population.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+    private uint _populationAmount=1000;
+    private double 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void  
+}
