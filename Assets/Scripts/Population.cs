5e8984b14a254e28995f2d741289dc1c67857b3a
NekoIay <77748612+NekoIay@users.noreply.github.com>
2021-09-07 08:52:42 +0300

    Upd

---
+++ Assets/Scripts/Population.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+
+    CityPopulation cityPopulation;
+    VillagesPopulation villagesPopulation;
+
+    private void Start() 
+    {
+        cityPopulation = gameObject.AddComponent<CityPopulation>();
+        villagesPopulation = gameObject.AddComponent<VillagesPopulation>();
+    }
+
+    private void Update()
+    {
+        cityPopulation.CreateCityPopulation();
+        villagesPopulation.CreateVilPopulation();
+    }
+
+    public uint _Amount;
+}
+public class CityPopulation : Population 
+{
+    new public uint _Amount;
+
+    public void CreateCityPopulation() 
+    {
+        _Amount = 10;
+    } 
+}
+public class VillagesPopulation : Population 
+{
+    new public uint _Amount;
+
+    public void CreateVilPopulation() 
+    {
+        _Amount = 5;
+    }  
+}
