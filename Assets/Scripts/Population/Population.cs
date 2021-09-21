94fe4afd05b64c3d55bdce5e66753df9e81010d8
NekoIay <77748612+NekoIay@users.noreply.github.com>
2021-09-21 09:19:06 +0300

    MedecineUpdate
    Added Medicine and it influence at population increase speed.

--- Assets/Scripts/Population/Population.cs
+++ Assets/Scripts/Population/Population.cs
@@ -6,12 +6,14 @@ public class Population : MonoBehaviour
     public float _Amount;
     public float _Education;
     public float _GenerationSpeed;
-    //public float _Medcine;
+    public float _Medcine;
+    public float _MedcineSupply;
     public float _NecessarySatisfactedNeeds;
     //public float _LuxurySatisfactedNeeds;
     public float _PopulationIncrease;
     //public float _ImmigrationAttractiveness=1;// перерасчет добавить
     private const float _NecessaryNeeds = 0.001f;
+    private const float _MedcineBugetPerPerson = 0.01f;
     
     [SerializeField]
     private Island island;
@@ -22,25 +24,28 @@ public class Population : MonoBehaviour
     {
         island = gameObject.AddComponent<Island>();
         //population = gameObject.AddComponent<Population>();
-        CreatePopulation(Amount: 1000, Education: 1);
+        CreatePopulation(Amount: 1000, Education: 1,Medcine: 0.5f);
         NessosaryResources = new[] { island.Food };
+        _MedcineSupply = 1f;
     }
     private void Update()
     {
         if (Timer.SecondGone())
         {
-            Debug.Log($"P{_Amount} F: {island.Food._amount} Satisf: {_NecessarySatisfactedNeeds}");
+            Debug.Log($"P{_Amount} F: {island.Food._amount} M: {_Medcine} Money:{island.Money._amount}");
             PopulationNeedUpdate(NessosaryResources);
+            MedcineUpdate();
+
             PopulationIncrease();
         }
         
     }
 
-    public void CreatePopulation(uint Amount,float Education/*,float Medcine*/) 
+    public void CreatePopulation(uint Amount,float Education, float Medcine) 
     {
         _Amount = Amount;
         _Education = Education;
-        //_Medcine = Medcine;
+        _Medcine = Medcine;
         _NecessarySatisfactedNeeds = 1f;  //со старта нужд нет
         //_LuxurySatisfactedNeeds = 1;
         _PopulationIncrease = 0.01f;
@@ -52,7 +57,7 @@ public class Population : MonoBehaviour
     }
     public void PopulationIncrease() 
     {
-        _Amount += (_PopulationIncrease */*_Medcine* */ _Amount * _NecessarySatisfactedNeeds) * Time.deltaTime;   
+        _Amount += (_PopulationIncrease * _Medcine * _Amount * _NecessarySatisfactedNeeds) * Time.deltaTime;   
     }
     
     public float PopulationNeed(Resource[] ConsumableResources) 
@@ -73,5 +78,28 @@ public class Population : MonoBehaviour
             }
         }
         return (TotalNeedSatisfation / ConsumableResources.Length);
+    }
+    public void MedcineUpdate() // не тестировано
+    {
+        float PartMedcineSupply = 1;
+        if (island.Money._amount >= _MedcineSupply * _MedcineBugetPerPerson * _Amount)
+        {
+            island.Money._amount -= _MedcineSupply * _MedcineBugetPerPerson * _Amount;//списание денег за медицину
+        }
+        else 
+        {
+            PartMedcineSupply = island.Money._amount / (_MedcineSupply * _MedcineBugetPerPerson * _Amount);
+            island.Money._amount = 0;
+        }
+
+        float DeltaSupply = (PartMedcineSupply * _MedcineSupply)-_Medcine;//узнаем разницу уровня реальной медицины от оплаченной
+        if (DeltaSupply < 0.03f && DeltaSupply > -0.03f)
+        {
+            _Medcine = PartMedcineSupply* _MedcineSupply;
+        }
+        else
+        {
+            _Medcine += DeltaSupply / 10;// зачисляется 10% до расчитываемого уровня
+        }
     }
 }
