2ea769009b0204f33d79c91c40f8dca8f134ebeb
the-17th-fox <nikitos171002@yandex.by>
2021-11-02 12:53:45 +0300

    Change subsidy UI numbers from N% style to N.N% style

--- Assets/UI/Scripts/SliderScript.cs
+++ Assets/UI/Scripts/SliderScript.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using System;
 
 public class SliderScript : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private Text _sliderValueText;
 
-    [SerializeField] Image _fill;
-    //[SerializeField] private Color _Full;
-    //[SerializeField] private Color _50upTo75Color;
-    //[SerializeField] private Color _25upTo50Color = Color.He;
-    //[SerializeField] private Color _0upTo25Color = Color.red;
+    [SerializeField] Image _fillColor;
 
     private void Start()
     {
         _slider.onValueChanged.AddListener((v) =>
         {
-            _fill.color = Color.Lerp(Color.red, Color.green, _slider.value / 100 + 0.2f);
-
-
-            _sliderValueText.text = $"{v.ToString("0")} %";
+            _fillColor.color = Color.Lerp(Color.red, Color.green, _slider.value / 100 + 0.2f);
+            _sliderValueText.text = $"{Math.Round(v,1)}%";
         });
     }
 }
